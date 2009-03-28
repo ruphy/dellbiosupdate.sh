6cf60f6df2327d5b43e70ff596f6c4aec970b1f1
Gaetano Andrea Callea <callea.gaetano.andrea@gmail.com>
2009-03-28 17:47:06 +0100

    first fixing release: 0.1.1; README modified. added VERSION_HISTORY file

--- dellbiosupdate.sh
+++ dellbiosupdate.sh
@@ -12,16 +12,24 @@
 #############################################################################################################
 ##                                                                                                         ##
 ##      Name:           dellbiosupdate.sh                                                                  ##
-##      Version:        0.1.0                                                                              ##
-##      Date:           Thu, Mar 26 2009                                                                   ##
+##      Version:        0.1.1                                                                              ##
+##      Date:           Sat, Mar 28 2009                                                                   ##
 ##      Author:         Callea Gaetano Andrea (aka cga)                                                    ##
 ##      Contributors:                                                                                      ##
-##      Language:      BASH                                                                               ##
+##      Language:       BASH                                                                               ##
 ##      Location:       http://github.com/cga/dellbiosupdate.sh/tree/master                                ##
 ##                                                                                                         ##
 #############################################################################################################
 
 ## let's roll!!!
+
+## the script has to be run as root, let's make sure of that:
+if [[ ${EUID} != 0 ]] ; then
+	echo
+	echo "You must run this script as root!! See FAQs in REAMDE for information"
+	echo
+	exit 1
+fi
 
 ## here the scripts checks if the needed tools are installed:
 if which dellBiosUpdate curl html2text >/dev/null 2>&1 ; then
@@ -32,30 +40,30 @@ else
 	echo "Either libsmbios, html2text or curl was NOT found! should I install it for you?"
 	echo
 
-	## .........you get prompted to install libsmbios for you specific distro:
-	select distro in "Debian, Ubuntu and derivatives" "Red Hat, Fedora, CentOS and derivatives" "SuSE, OpenSuSE and derivatives" "Arch and derivatives" "Gentoo and derivatives" "Quit, I will install it myself" "Ok, I'm done installing. Let's move on!" ; do
-	if [ "$distro" == "Debian, Ubuntu and derivatives" ] ; then
+	## .........you get prompted to install libsmbios for your specific DISTRO:
+	select DISTRO in "Debian, Ubuntu and derivatives" "Red Hat, Fedora, CentOS and derivatives" "SuSE, OpenSuSE and derivatives" "Arch and derivatives" "Gentoo and derivatives" "Quit, I will install it myself" "Ok, I'm done installing. Let's move on!" ; do
+	if [ "$DISTRO" == "Debian, Ubuntu and derivatives" ] ; then
 		apt-get install libsmbios-bin curl html2text
 	fi
-	if [ "$distro" == "Red Hat, Fedora, CentOS and derivatives" ] ; then
+	if [ "$DISTRO" == "Red Hat, Fedora, CentOS and derivatives" ] ; then
 		yum install firmware-addon-dell libsmbios curl html2text
 	fi
-	if [ "$distro" == "SuSE, OpenSuSE and derivatives" ] ; then
+	if [ "$DISTRO" == "SuSE, OpenSuSE and derivatives" ] ; then
 		zypper install libsmbios-bin curl html2text
 	fi
-	if [ "$distro" == "Arch and derivatives" ] ; then
+	if [ "$DISTRO" == "Arch and derivatives" ] ; then
 		pacman -S libsmbios curl html2text
 	fi
-	if [ "$distro" == "Gentoo and derivatives" ] ; then
+	if [ "$DISTRO" == "Gentoo and derivatives" ] ; then
 		emerge -av libsmbios curl html2text
 	fi
-	if [ "$distro" == "Quit, I will install it myself" ] ; then
+	if [ "$DISTRO" == "Quit, I will install it myself" ] ; then
 		echo
 		echo "Please install libsmbios, html2text and curl"
 		echo
-		exit 1
+		exit 2
 	fi
-	if [ "$distro" == "Ok, I'm done installing. Let's move on!" ] ; then
+	if [ "$DISTRO" == "Ok, I'm done installing. Let's move on!" ] ; then
 		break 1	
 	fi
 done
@@ -84,33 +92,45 @@ echo "These are the available BIOS updates available for your ${COMPUTER}:"
 echo
 
 ## just to make sure PS3 doesn't get changed forever:
-oldPS3=$PS3 
+OLDPS3=$PS3 
 COLUMNS=10
-PS3=$'\nNote that you actually *can* install the latest BIOS update without updating the immediately subsequent version.\n\nChoose the BIOS Version you want to install by typing the corresponding number: ' ; 
-select BIOS_VERSION in "${a[@]}" ; do [[ $BIOS_VERSION ]] && echo && break ; done
+PS3=$'\nNote that you actually *can* install the latest BIOS update without updating the immediately subsequent version.\n\nChoose the BIOS Version you want to install by typing the corresponding number: '
+select BIOS_VERSION in "${a[@]}" "I already have BIOS Version ${BIOS_VERSION_BASE} installed" ; do 
+## we offer option to quit script on user will if BIOS Version is already installed
+	if [ "$BIOS_VERSION" == "I already have BIOS Version ${BIOS_VERSION_BASE} installed"  ] ; then
+	echo 
+	echo "Thanks for using this script; Now you know you have a tool to check if new BIOS Version are available ;)"
+	echo 
+	exit 3
+	
+	elif [[ $BIOS_VERSION ]]; then 
+	break; 
+	fi 
+done
+echo 
 COLUMNS=
-PS3=$oldPS3
+PS3=$OLDPS3
 
 ## now that we have all the data, we need to set the URL to download the right BIOS:
 URL=http://linux.dell.com/repo/firmware/bios-hdrs/system_bios_ven_0x1028_dev_${SYSTEM_ID}_version_${BIOS_VERSION}/bios.hdr
 
 ## if an unknown bios.hdr version exist then mv it and append $DATE; finally download the bios.hdr file with the version saved in the file name:
-if [ -f "/root/bios.hdr" ] ; then
-	echo "I found an existing BIOS file (/root/bios.hdr) of which I don't know the version and I'm going to back it up as /root/bios-$(date +%Y-%m-%d).hdr"
+if [ -f "~/bios.hdr" ] ; then
+	echo "I found an existing BIOS file (~/bios.hdr) of which I don't know the version and I'm going to back it up as ~/bios-$(date +%Y-%m-%d).hdr"
 	echo
 	sleep 1
-        mv /root/bios.hdr /root/bios-$(date +%Y-%m-%d).hdr
+        mv ~/bios.hdr ~/bios-$(date +%Y-%m-%d).hdr
 	sleep 1
-	echo "Downloading selected BIOS Version ${BIOS_VERSION} for your ${COMPUTER} and saving it as /root/bios-${BIOS_VERSION}.hdr"
+	echo "Downloading selected BIOS Version ${BIOS_VERSION} for your ${COMPUTER} and saving it as ~/bios-${BIOS_VERSION}.hdr"
 	echo
 	sleep 1
-curl ${URL} -o /root/bios-${BIOS_VERSION}.hdr
+curl ${URL} -o ~/bios-${BIOS_VERSION}.hdr
 echo
 else
-	echo "Downloading selected BIOS Version ${BIOS_VERSION} for your ${COMPUTER} and saving it as /root/bios-${BIOS_VERSION}.hdr"
+	echo "Downloading selected BIOS Version ${BIOS_VERSION} for your ${COMPUTER} and saving it as ~/bios-${BIOS_VERSION}.hdr"
 	echo
 	sleep 1
-curl ${URL} -o /root/bios-${BIOS_VERSION}.hdr
+curl ${URL} -o ~/bios-${BIOS_VERSION}.hdr
 echo
 fi
 
@@ -119,15 +139,15 @@ echo "Checking if BIOS Version ${BIOS_VERSION} for your ${COMPUTER} is valid....
 sleep 3
 echo
 ## if not the script will exit and remove the downloaded BIOS:
-dellBiosUpdate -t -f /root/bios-${BIOS_VERSION}.hdr >/dev/null 2>&1; STATUS_FAIL=$?
+dellBiosUpdate -t -f ~/bios-${BIOS_VERSION}.hdr >/dev/null 2>&1; STATUS_FAIL=$?
 if (( ${STATUS_FAIL} > 0 )) ; then
 	echo "WARNING: BIOS HDR file BIOS version appears to be less than or equal to current BIOS version."
 	echo "This may result in bad things happening!!!!"
 	echo
-	rm -f /root/bios-${BIOS_VERSION}.hdr
-	echo "The downloaded /root/bios-${BIOS_VERSION}.hdr has been deleted."
+	rm -f ~/bios-${BIOS_VERSION}.hdr
+	echo "The downloaded ~/bios-${BIOS_VERSION}.hdr has been deleted."
 	echo
-	exit 2
+	exit 4
 
 ## if BIOS is valid we load the needed DELL module and proceed with the update:
 else 
@@ -137,7 +157,7 @@ else
 	echo "The necessary 'dell_rbu' module has been loaded"
 	echo
 ## the actual update:
-	dellBiosUpdate -u -f /root/bios-${BIOS_VERSION}.hdr
+	dellBiosUpdate -u -f ~/bios-${BIOS_VERSION}.hdr
 	echo
 fi
 
